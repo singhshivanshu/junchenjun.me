535e0878f231c87a9f72aba0a718d8c2dcbfe535
Jun Chen <junchen.cq@gmail.com>
2020-03-03 17:02:19 -0500

    Styles fix

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { useMediaQuery } from 'react-responsive'
 import MobileMenu from './MobileMenu/index'
 import CommenMenu from './CommenMenu/index'
 
 const Menu = props => {
     const { setPage } = props
-    const isMobile = useMediaQuery({ maxWidth: 500 })
 
-    return isMobile ? (
-        <MobileMenu setPage={setPage} />
-    ) : (
-        <CommenMenu setPage={setPage} />
+    return (
+        <div>
+            <MobileMenu setPage={setPage} />
+            <CommenMenu setPage={setPage} />
+        </div>
     )
 }
 
