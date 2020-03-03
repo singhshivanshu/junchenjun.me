a7d54ee565b6d291103e80355cbb208a284ac0b4
Jun Chen <junchen.cq@gmail.com>
2020-03-03 17:26:09 -0500

    Disable mobile zooming on mobile

--- src/utilities/Helmet/index.jsx
+++ src/utilities/Helmet/index.jsx
@@ -18,6 +18,10 @@ const AppHelmet = () => {
                 name="apple-mobile-web-app-status-bar-style"
                 content="#121620"
             />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            />
         </Helmet>
     )
 }
